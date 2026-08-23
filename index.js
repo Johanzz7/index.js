cb8c3e5a2f33a08f76f220610cf737b6dada8b0c
Johanzz7 <acerojohan07@gmail.com>
2026-08-23 01:22:14 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  "name": "umi-asanagui",
+  "version": "1.0.0",
+  "description": "Bot de WhatsApp Umi Asanagui",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "baileys": "7.0.0-rc14",
+    "pino": "^9.7.0",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
